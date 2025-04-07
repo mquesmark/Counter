34b93451abbe470acb02083cd7bdfc61e3d55500
Maxim <maximgv@icloud.com>
2025-04-07 18:31:00 +0300

    Выполнено расширенное задание: добавлены кнопки + и -, кнопка сброса и история действий. И еще моя кошечка на иконку приложения, чтобы радовала глаз. Ее зовут Рыся!

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,18 +10,57 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var counterText: UILabel! // Цифра счетчика
-    @IBOutlet weak var plusButton: UIButton! // Кнопка +1
+    @IBOutlet weak var plusButton: UIButton! // Кнопка +
+    @IBOutlet weak var minusButton: UIButton! // Кнопка –
+    @IBOutlet weak var resetButton: UIButton! // Кнопка сброса
+    @IBOutlet weak var hystoryUITextView: UITextView! // Поле с историей
+    
+
     private var counterValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        minusButton.tintColor = .blue
+        plusButton.tintColor = .red
+        counterValue = 0
     }
 
-     @IBAction func buttonIsTouched() {
+    private func updateCounter () -> Void { // Функция обновления текста счётчика
+        counterText.text = "Значение счётчика: \(counterValue)"
+    }
+    
+    private func checkDate() -> String {
+        let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm:ss dd.MM.yyyy"
+return formatter.string(from: Date())
+    }
+     @IBAction func plusButtonIsTouched() {
         counterValue += 1
-         counterText.text = "Значение счётчика: \(counterValue)"
+         updateCounter()
+         hystoryUITextView.text += "\n• [\(checkDate())]: значение изменено на +1"
+        
+    }
+    
+    @IBAction func minusButtonIsTouched() {
+        if counterValue > 0 {
+            counterValue -= 1
+            updateCounter()
+            hystoryUITextView.text += "\n• [\(checkDate())]: значение изменено на -1"
+        }
+        else {
+            hystoryUITextView.text += "\n• [\(checkDate())]: попытка уменьшить значение счётчика ниже 0"
+        }
+    }
+    
+    @IBAction func resetTapped(_ sender: Any) {
+        counterValue = 0
+        updateCounter()
+        hystoryUITextView.text += "\n• [\(checkDate())]: значение сброшено"
     }
     
 }
 
+
+
+// пасхалка (да, мне нечего делать)
