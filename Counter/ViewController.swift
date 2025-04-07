d7bb32e86819c2a90891b9e97e3edd5428a211d1
Maxim <maximgv@icloud.com>
2025-04-07 13:00:38 +0300

    Выполнено базовое задание: создан счётчик, добавлен текст, кнопка, счетчик научен своим функциям

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterText: UILabel! // Цифра счетчика
+    @IBOutlet weak var plusButton: UIButton! // Кнопка +1
+    private var counterValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+     @IBAction func buttonIsTouched() {
+        counterValue += 1
+         counterText.text = "Значение счётчика: \(counterValue)"
+    }
+    
 }
 
