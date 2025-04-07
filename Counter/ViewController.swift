fd273e9c78a1f31a5b101fd19ea12204c13b0381
Maxim <maximgv@icloud.com>
2025-04-07 12:38:41 +0300

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Максим on 07.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
